4fa7068f92c00ca45915acc5f1dc1c11a33f0dfe
karola15134 <karolina.sz1993@gmail.com>
2018-03-16 15:20:39 +0100

    spring security

--- src/main/java/com/mycompany/jpafllyway/App.java
+++ src/main/java/com/mycompany/jpafllyway/App.java
@@ -7,10 +7,13 @@ package com.mycompany.jpafllyway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.flywaydb.core.Flyway;
 
+/**
+ *
+ * @author Karo
+ */
 @SpringBootApplication
-public class App {
+public class App   {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
